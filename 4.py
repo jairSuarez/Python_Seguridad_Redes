30eb9d18bf002150a9c01f7e3f0a3bd260b42727
Jair Suarez <jairsuarez11@gmail.com>
2022-10-21 19:53:34 -0500

    Ejercicio 4 clasifica entre Vocal y No Vocal

---
+++ 4.py
@@ -0,0 +1,19 @@
+'''
+4- Escribir una función que tome un 
+carácter y devuelva True si es una vocal, 
+de lo contrario devuelve False.
+'''
+
+def esVocal(caracter = ""):
+    caracter = caracter.lower()
+    vocales = ['a','e','i','o','u']
+    if caracter in vocales:
+        return True
+    return False
+
+caracter = input('Ingresa un carcater: ')
+
+if esVocal(caracter):
+    print(f'"{caracter}" es una vocal.')
+else:
+    print(f'"{caracter}" no es una vocal.')
