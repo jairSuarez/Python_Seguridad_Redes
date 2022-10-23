74ccbe65a13fba71c78cdf0941b64d54d9ee331c
Jair Suarez <jairsuarez11@gmail.com>
2022-10-23 17:37:08 -0500

    Ejercicios 5, suma y multiplica todos los numeros de una lista

---
+++ 5.py
@@ -0,0 +1,36 @@
+'''
+5- Escribir una funcion sum() y una función multip() 
+que sumen y multipliquen respectivamente todos los números de una lista.
+Por ejemplo: sum([1,2,3,4]) debería devolver 10 y 
+multip([1,2,3,4]) debería devolver 24
+'''
+
+def sum(lista=[]):
+    resultado = 0
+    for i in lista:
+        resultado += i
+    return resultado
+
+def multip(lista=[]):
+    resultado = 1
+    for i in lista:
+        resultado *= i
+    return resultado 
+
+numeros = []
+
+while True:
+    num = input('Ingresa un numero, s=salir. R = ')
+    if num.lower() == 's':
+        break
+    else:
+        try:
+            num = int(num)
+            numeros.append(num)
+        except:
+            continue
+print('Numeros ingresados: ', numeros)
+print(f'Suma de todos los números: {sum(numeros)}')
+print(f'Multiplicación de todos los números: {multip(numeros)}')
+
+
