c99a27fce334603b87c618ec74fef7ca058334dd
jairSuarez <57118118+jairSuarez@users.noreply.github.com>
2022-11-05 16:05:02 -0600

    Codigo de Servidor TCP

---
+++ server.py
@@ -0,0 +1,20 @@
+from email import message
+from http import client
+import socket
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = socket.gethostname() # Ip del host
+port = 444
+
+serversocket.bind((host, port))
+
+serversocket.listen(3) # Máximo numero de peticiones a atender
+
+while True:
+    clientsocket, address = serversocket.accept() #aceptar la información TCP proveniente del cliente
+    print("Conexión recibida de " + str(address))
+    message = 'Hola, gracias por conectarte al servidor!' + "\r\n"
+    clientsocket.send(message.encode('ascii')) # Mensaje para el cliente cuando se conexte exitosamente
+    clientsocket.close()
+
